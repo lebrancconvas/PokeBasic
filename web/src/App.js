924493b5b5b41f49cbc7c6d094921be0750fbec4
lebrancconvas <lebrancconvas@gmail.com>
2021-11-24 11:58:49 +0700

    🚚 Resource Clear.

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code> src / App.js </code> and save to reload. 
         </p>
         <a
           className="App-link"
